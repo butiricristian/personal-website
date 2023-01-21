5f590f50762afb404fc5ba71d56be31acdc3c2e8
cristian_butiri <cristian.butiri@andopen.co>
2023-01-21 17:56:00 +0200

    Small fixes

--- components/sections/HomeSection.js
+++ components/sections/HomeSection.js
@@ -1,7 +1,8 @@
-import styles from '../../styles/Home.module.css'
-import Button from '../common/Button'
-import development from '../../public/Development2.svg'
-import Image from 'next/image'
+import styles from "../../styles/Home.module.css";
+import Button from "../common/Button";
+import development from "../../public/Development2.svg";
+import Image from "next/image";
+import "../../styles/Animation.module.css";
 
 export default function HomeSection() {
   return (
@@ -10,11 +11,13 @@ export default function HomeSection() {
         <h3>Hi, my name is</h3>
         <h1>Cristian Butiri</h1>
         <h4>Let's grow your business into the digital world</h4>
-        <Button style={{marginTop: '4rem'}} size="lg">Get in touch</Button>
+        <Button style={{ marginTop: "4rem" }} size="lg">
+          Get in touch
+        </Button>
       </div>
       <div className={styles.rightColumn}>
-        <Image src={development} alt="development" className={styles.image}/>
+        <Image src={development} alt="development" className={styles.image} />
       </div>
     </section>
-  )
+  );
 }
